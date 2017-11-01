af5b1ffa64218c25375ce25d577c243b2af5906b
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 20:07:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value.collect do |key, value|
-    return
+  name_hash.collect do |key, value|
+    return key
 end
