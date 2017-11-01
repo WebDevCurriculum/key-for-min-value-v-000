b0437c9c70436f1b9e5179d5680212f413b86abc
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 20:16:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  hash.each do |key, value|
+  hash.collect do |key, value|
     if value < lowest_value
       lowest_value = value
       lowest_key = key
@@ -12,4 +12,3 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
-
